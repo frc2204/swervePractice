9cc12f3a79d7b8508da98017026d62e3f3c84797
plainriceeater <therealasher09@gmail.com>
2025-01-11 13:14:23 -0800

    finished the arm positioning

---
+++ Arm.kt
@@ -0,0 +1,16 @@
+object Arm {
+
+
+
+   enum class Position(
+        val elbowPosition:Double,
+        val wristPosition:Double,)
+    {
+
+        Position1(0.1,0.2),
+        Position2(0.4,0.5)
+    }
+
+}
+
+
